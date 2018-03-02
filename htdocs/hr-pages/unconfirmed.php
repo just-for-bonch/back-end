7c29714471751d65489424d37e44fbedc8ba64b5
vikapitoshka <kapitonenko.vick@ya.ru>
2018-03-02 11:08:07 +0300

    uncomf php

---
+++ htdocs/hr-pages/unconfirmed.php
@@ -0,0 +1,145 @@
+<?php session_start(); include("../connect.php"); ?>
+<?php include( '../header.php' ); ?>
+<?php include( '../sidebar.php' ); ?>
+
+  <div class="container"><!-- start main content -->
+    <div class="row wrapper">     
+      <div class="col col-10">
+        <h2>Неподтверждённые пользователи</h2>
+        <p class="company">Ваша компания: Just for Bonch</p>
+        <div class="users row">
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+          <div class="user col col-6"><!-- block of user -->
+            <div class="user-delete">
+              <a href="#"><img src="../img/delete.svg"></a>
+            </div>
+            <div class="user-image">
+              <img src="../img/1d.png">
+            </div>
+            <div class="user-meta-btns">
+              <div class="user-meta">
+                <p class="name">Имя Фамилия Отчество</p>
+                <p>Дата рождения: 26.07.1987</p>
+              </div>
+              <div class="btn-group row">
+                <button class="col btn-yellow">Подтвердить как работника</button>
+                <button class="col btn-green">Подтвердить как руководителя</button>
+              </div>
+            </div>
+          </div><!-- end block of user -->
+        </div><!-- end blocks -->
+        
+      </div><!-- end col-10 -->
+    </div><!-- end row -->
+  </div><!-- end main content -->
+
+<?php include( '../footer.php' ); ?>
+</body>
+</html>
