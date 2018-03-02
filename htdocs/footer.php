f00ccefc0b9bd54e3bebb8ef7583cf89a00b7848
vikapitoshka <kapitonenko.vick@ya.ru>
2018-03-02 10:43:53 +0300

    add layouts

---
+++ htdocs/footer.php
@@ -0,0 +1,9 @@
+  <footer><!-- start footer -->
+    <div class="container"> 
+      Copyright (c) Just for Bonch 2018
+    </div>
+  </footer><!-- end footer -->
+    
+  </div>
+<script data-rocketsrc="https://cdn.jsdelivr.net/npm/vue"></script>
+<script src="js/script.js"></script>
---
+++ htdocs/sidebar.php
@@ -0,0 +1,8 @@
+  <div class="sidebar"><!-- start sidebar -->
+    <ul class="block-links">
+      <li><a href="../hr-pages/profile_hr.php">Профиль</a></li>
+      <li><a href="#">Календарь</a></li>
+      <li><a href="#">Задания</a></li>
+      <li><a href="#">Рейтинг</a></li>
+    </ul>
+  </div><!-- end sidebar -->
